4cfac73a2ab35e47cea9a889b7e0503d69062d1b
Heath Snow <heathsnow@gmail.com>
2019-07-19 13:40:38 -0700

    Update nerd-tree.sh
    update nerd-tree url

--- script/lib/nerd-tree.sh
+++ script/lib/nerd-tree.sh
@@ -3,7 +3,7 @@
 logn 'Installing NERD_tree vim plugin...'
 
 if [ ! -d "$HOME/.vim/pack/plugins/start/NERD_tree/" ]; then
-  curl -o /tmp/NERD_tree.zip http://www.vim.org/scripts/download_script.php?src_id=23731
+  curl -o /tmp/NERD_tree.zip https://www.vim.org/scripts/download_script.php?src_id=23731
   unzip -q /tmp/NERD_tree.zip -d $HOME/.vim/pack/plugins/start/NERD_tree/
   rm /tmp/NERD_TREE.zip
   vim -u NONE -c "helptags $HOME/.vim/pack/plugins/start/NERD_tree/doc" -c -q
