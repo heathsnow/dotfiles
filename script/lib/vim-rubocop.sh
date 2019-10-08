5d4eda7e101aea9a7774282ab5181e167fac3c77
heathsnow <hsnow@impinj.com>
2019-10-08 11:51:09 -0700

    fix git url for vim-rubocop

--- script/lib/vim-rubocop.sh
+++ script/lib/vim-rubocop.sh
@@ -3,7 +3,7 @@
 logn 'Installing vim-rubocop plugin...'
 
 if [ ! -d "$HOME/.vim/pack/plugins/start/vim-rubocop/" ]; then
-  git clone git@github.com:hashivim/vim-rubocop.git $HOME/.vim/pack/plugins/start/vim-rubocop
+  git clone git@github.com:ngmy/vim-rubocop.git $HOME/.vim/pack/plugins/start/vim-rubocop
   vim -u NONE -c "helptags $HOME/.vim/pack/plugins/start/vim-rubocop/doc" -c -q
 fi
 
